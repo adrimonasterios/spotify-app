d170cd162ad780d5e34d83aac6219cfa10fd717f
Adrian Monasterios Gutierrez <adri.suscripciones@gmail.com>
2023-08-23 00:18:34 -0400

    Added button component

---
+++ app/_components/common/Button.tsx
@@ -0,0 +1,40 @@
+import { ButtonHTMLAttributes, LegacyRef } from "react";
+import { classNames } from "@/_utils/helpers";
+
+const buttonVariants = {
+  primary: "bg-primary-400  text-white  hover:bg-primary-500",
+  ghost:
+    "shadow-none bg-transparent text-gray-500  cursor-pointer hover:text-gray-700",
+};
+
+export type ButtonProps = Partial<ButtonHTMLAttributes<HTMLElement>> & {
+  text: string | JSX.Element;
+  onClick?: (e: MouseEvent) => void;
+  variant?: "primary" | "ghost";
+};
+
+const Button = ({
+  text,
+  onClick,
+  variant,
+  className,
+  disabled,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      className={classNames(
+        `inline-flex items-center rounded border border-transparent px-2.5 py-1.5 text-xs font-semibold shadow-sm justify-center focus:outline-none ${
+          buttonVariants[variant || "primary"]
+        }`,
+        className
+      )}
+      onClick={disabled ? undefined : onClick}
+      {...props}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default Button;
