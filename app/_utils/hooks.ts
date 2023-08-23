5633b469d7a77834c4bdbda29689e0828c54fc97
Adrian Monasterios Gutierrez <adri.suscripciones@gmail.com>
2023-08-22 20:58:45 -0400

    added hooks to fetch easier

--- app/_utils/hooks.ts
+++ app/_utils/hooks.ts
@@ -1,3 +1,5 @@
+"use client";
+import { useSpotifyAuthentication } from "@/_components/providers/SpotifyAuthenticationProvider";
 import { useEffect, useState } from "react";
 
 export function useDebounce<T>(
@@ -21,3 +23,48 @@ export function useDebounce<T>(
 
   return { debouncedValue, loading };
 }
+
+export const useFetchAlbum = () => {
+  const { token } = useSpotifyAuthentication();
+  const [album, setAlbum] = useState<{ [key: string]: any }>({});
+
+  const fetchAlbum = async (id: string) => {
+    const response = await fetch(`/api/albums/${id}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const res = await response.json();
+    setAlbum(res);
+  };
+
+  return { album, fetchAlbum };
+};
+
+export const useFetchArtist = () => {
+  const { token } = useSpotifyAuthentication();
+  const [artist, setArtist] = useState<{ [key: string]: any }>({});
+
+  const fetchArtist = async (id: string) => {
+    const response = await fetch(`/api/artists/${id}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const res = await response.json();
+    setArtist(res);
+  };
+
+  return { artist, fetchArtist };
+};
+
+export const useFetchTrack = () => {
+  const { token } = useSpotifyAuthentication();
+  const [track, setTrack] = useState<{ [key: string]: any }>({});
+
+  const fetchTrack = async (id: string) => {
+    const response = await fetch(`/api/tracks/${id}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const res = await response.json();
+    setTrack(res);
+  };
+
+  return { track, fetchTrack };
+};
